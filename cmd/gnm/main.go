f37372e0be8bfd42b898ed35f48cd5a6c032a067
Rafael <104688305+rafa-dot-el@users.noreply.github.com>
2023-11-11 17:29:53 +0100

    feat: Plotting

--- cmd/gnm/main.go
+++ cmd/gnm/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/guptarohit/asciigraph"
+	"golang.org/x/term"
 	"io/ioutil"
 	"os"
 	"os/signal"
@@ -85,18 +87,20 @@ var (
 	onlyRX               = false
 	onlyTX               = false
 	hideNetworkInterface = false
+	plot                 = false
 	outputFreq           = defaultInterval
 	interfaceName        string
 	state                NetworkStats
 )
 
 func init() {
-	flag.BoolVar(&allStats, "all", false, "Show all network link statistics ")
+	flag.BoolVar(&allStats, "all", true, "Show all network link statistics ")
 	flag.BoolVar(&showCount, "count", false, "Show statistics about packet count")
 	flag.BoolVar(&showTransfer, "transfer", false, "Show statistics about total bytes transferred")
 	flag.BoolVar(&showDropped, "dropped", false, "Show statistics about dropped packets")
 	flag.BoolVar(&onlyRX, "only-rx", false, "Show only received packets statistics")
 	flag.BoolVar(&onlyTX, "only-tx", false, "Show only sent packets statistics")
+	flag.BoolVar(&plot, "plot", false, "Plot the current statistics in a chart, only allows one statistic (RX or TX) and one metric (count/transfer/drops)")
 	flag.BoolVar(&hideNetworkInterface, "hideNetworkInterface", false, "Don't print the network interface name")
 	flag.IntVar(&outputFreq, "output-frequency", defaultInterval, "Output frequency in seconds (output will be averaged to the interval)")
 	flag.Usage = func() {
@@ -104,6 +108,58 @@ func init() {
 		fmt.Println("repository: https://github.com/rafa-dot-el/gonetmon")
 		fmt.Println("Options:")
 		flag.PrintDefaults()
+	}
+}
+
+// Extract only the required field for plotting
+func extractData(input NetworkStats) uint64 {
+	if allStats {
+		return 0
+	}
+
+	if onlyRX {
+		if showCount {
+			return input.packetsRX
+		}
+		if showTransfer {
+			return input.bytesRX
+		}
+		if showDropped {
+			return input.droppedRX
+		}
+
+	} else if onlyTX {
+		if showCount {
+			return input.packetsTX
+		}
+		if showTransfer {
+			return input.bytesTX
+		}
+		if showDropped {
+			return input.droppedTX
+		}
+	}
+	return 0
+}
+
+func plotterConsumer(input <-chan NetworkStats) {
+	const maxBufferSize = 60
+	data := make([]float64, 0, maxBufferSize)
+	for v := range input {
+		if len(data) >= maxBufferSize {
+			data = data[1:]
+		}
+		data = append(data, float64(extractData(v)))
+		//fmt.Print("\033[H\033[2J")
+		fmt.Printf("\x1b[2J")
+		width, height, err := term.GetSize(0)
+		if err != nil {
+			return
+		}
+		height = height - 4
+		width = width - 10
+		graph := asciigraph.Plot(data, asciigraph.Height(height), asciigraph.Width(width))
+		fmt.Println(graph)
 	}
 }
 
@@ -121,9 +177,44 @@ func main() {
 		allStats = false
 	}
 
+	if plot {
+		metrics := 0
+		if allStats {
+			metrics = 3
+		}
+		if showCount {
+			metrics += 1
+		}
+		if showTransfer {
+			metrics += 1
+		}
+		if showDropped {
+			metrics += 1
+		}
+		if metrics > 1 || !(onlyRX || onlyTX) {
+			err := fmt.Errorf("plotting only allows one statistic (RX or TX) and one metric (count/transfer/drops)")
+			fmt.Println(err)
+			os.Exit(255)
+		}
+		if !term.IsTerminal(0) {
+			err := fmt.Errorf("Plotting requires a valid terminal")
+			fmt.Println(err)
+			os.Exit(255)
+		}
+	}
+
 	// Setup signal handling for Ctrl+C
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Signals for plots
+	metricChannel := make(chan NetworkStats)
+
+	if plot {
+		go plotterConsumer(metricChannel)
+	} else {
+		go stdioConsumer(metricChannel)
+	}
 
 	// Start monitoring loop
 	ticker := time.NewTicker(time.Duration(outputFreq) * time.Second)
@@ -132,7 +223,7 @@ func main() {
 	if error != nil {
 		fmt.Printf("Error reading interface %s data: %s", interfaceName, error)
 	}
-	go monitorNetworkStats(ticker, quit)
+	go monitorNetworkStats(ticker, quit, metricChannel)
 
 	// Wait for Ctrl+C
 	<-signalChannel
@@ -143,11 +234,11 @@ func main() {
 	fmt.Println("Program terminated.")
 }
 
-func monitorNetworkStats(ticker *time.Ticker, quit chan bool) {
+func monitorNetworkStats(ticker *time.Ticker, quit chan bool, statsChannel chan<- NetworkStats) {
 	for {
 		select {
 		case <-ticker.C:
-			printNetworkStats()
+			processNetworkStats(statsChannel)
 		case <-quit:
 			return
 		}
@@ -179,12 +270,71 @@ func convertUnit(value uint64, unit string) (float64, string) {
 	return resultValue, resultUnit
 }
 
-func printNetworkStats() {
+func stdioConsumer(input <-chan NetworkStats) {
+	for diff := range input {
+		// Print the statistics
+		if !hideNetworkInterface {
+			fmt.Printf("%s: ", interfaceName)
+		}
 
-	// Print the statistics
-	if !hideNetworkInterface {
-		fmt.Printf("%s: ", interfaceName)
+		needComma := allStats
+		if allStats || showCount {
+			unit := "p/s"
+			if onlyRX {
+				packetsRX, unitRX := convertUnit(diff.packetsRX, unit)
+				fmt.Printf("Packets %0.1f %s", packetsRX, unitRX)
+			} else if onlyTX {
+				packetsTX, unitTX := convertUnit(diff.packetsTX, unit)
+				fmt.Printf("Packets %0.1f %s", packetsTX, unitTX)
+			} else {
+				packetsRX, unitRX := convertUnit(diff.packetsRX, unit)
+				packetsTX, unitTX := convertUnit(diff.packetsTX, unit)
+				fmt.Printf("Packets RX:%0.1f %s TX:%0.1f %s", packetsRX, unitRX, packetsTX, unitTX)
+			}
+			needComma = true
+		}
+		if allStats || showTransfer {
+			if needComma {
+				fmt.Print(",")
+			}
+			unit := "b/s"
+			if onlyRX {
+				bytesRX, unitRX := convertUnit(diff.bytesRX, unit)
+				fmt.Printf("Data %0.1f %s", bytesRX, unitRX)
+			} else if onlyTX {
+				bytesTX, unitTX := convertUnit(diff.bytesTX, unit)
+				fmt.Printf("Data %0.1f %s", bytesTX, unitTX)
+			} else {
+				bytesRX, unitRX := convertUnit(diff.bytesRX, unit)
+				bytesTX, unitTX := convertUnit(diff.bytesTX, unit)
+				fmt.Printf("Data RX:%0.1f %s TX:%0.1f %s", bytesRX, unitRX, bytesTX, unitTX)
+			}
+			needComma = true
+		}
+
+		if allStats || showDropped {
+			if needComma {
+				fmt.Print(",")
+			}
+			unit := "d/s"
+
+			if onlyRX {
+				droppedRX, unitRX := convertUnit(diff.droppedRX, unit)
+				fmt.Printf("Drops %0.1f %s", droppedRX, unitRX)
+			} else if onlyTX {
+				droppedTX, unitTX := convertUnit(diff.droppedTX, unit)
+				fmt.Printf("Drops %0.1f %s", droppedTX, unitTX)
+			} else {
+				droppedRX, unitRX := convertUnit(diff.droppedRX, unit)
+				droppedTX, unitTX := convertUnit(diff.droppedTX, unit)
+				fmt.Printf("Drops RX:%0.1f %s TX:%0.1f %s", droppedRX, unitRX, droppedTX, unitTX)
+			}
+		}
+		fmt.Println()
 	}
+}
+
+func processNetworkStats(input chan<- NetworkStats) {
 
 	// Read network statistics from the specified files
 	newState := NetworkStats{}
@@ -198,60 +348,8 @@ func printNetworkStats() {
 	diff := state.NormalizeDiff(&newState, uint64(outputFreq))
 	state = newState
 
-	needComma := allStats
-	if allStats || showCount {
-		unit := "p/s"
-		if onlyRX {
-			packetsRX, unitRX := convertUnit(diff.packetsRX, unit)
-			fmt.Printf("Packets %0.1f %s", packetsRX, unitRX)
-		} else if onlyTX {
-			packetsTX, unitTX := convertUnit(diff.packetsTX, unit)
-			fmt.Printf("Packets %0.1f %s", packetsTX, unitTX)
-		} else {
-			packetsRX, unitRX := convertUnit(diff.packetsRX, unit)
-			packetsTX, unitTX := convertUnit(diff.packetsTX, unit)
-			fmt.Printf("Packets RX:%0.1f %s TX:%0.1f %s", packetsRX, unitRX, packetsTX, unitTX)
-		}
-		needComma = true
-	}
-	if allStats || showTransfer {
-		if needComma {
-			fmt.Print(",")
-		}
-		unit := "b/s"
-		if onlyRX {
-			bytesRX, unitRX := convertUnit(diff.bytesRX, unit)
-			fmt.Printf("Data %0.1f %s", bytesRX, unitRX)
-		} else if onlyTX {
-			bytesTX, unitTX := convertUnit(diff.bytesTX, unit)
-			fmt.Printf("Data %0.1f %s", bytesTX, unitTX)
-		} else {
-			bytesRX, unitRX := convertUnit(diff.bytesRX, unit)
-			bytesTX, unitTX := convertUnit(diff.bytesTX, unit)
-			fmt.Printf("Data RX:%0.1f %s TX:%0.1f %s", bytesRX, unitRX, bytesTX, unitTX)
-		}
-		needComma = true
-	}
+	input <- *diff
 
-	if allStats || showDropped {
-		if needComma {
-			fmt.Print(",")
-		}
-		unit := "d/s"
-
-		if onlyRX {
-			droppedRX, unitRX := convertUnit(diff.droppedRX, unit)
-			fmt.Printf("Drops %0.1f %s", droppedRX, unitRX)
-		} else if onlyTX {
-			droppedTX, unitTX := convertUnit(diff.droppedTX, unit)
-			fmt.Printf("Drops %0.1f %s", droppedTX, unitTX)
-		} else {
-			droppedRX, unitRX := convertUnit(diff.droppedRX, unit)
-			droppedTX, unitTX := convertUnit(diff.droppedTX, unit)
-			fmt.Printf("Drops RX:%0.1f %s TX:%0.1f %s", droppedRX, unitRX, droppedTX, unitTX)
-		}
-	}
-	fmt.Println()
 }
 
 func readUint64FromFile(filePath string) (uint64, error) {
